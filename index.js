26d3cd7a393b6be6dc9e7f6097f4b82b4a5ec12a
muneebsafdar <safdarmuneeb95@gmail.com>
2026-06-29 23:43:47 -0700

    new test

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
 import express from 'express'
 
-const app=express()
+const app = express()
 
-const port =3000
-const hostname='127.0.0.1'
+const port = 3000
+const hostname = '127.0.0.1'
 
 
 
 app.use(express.json());
 
-let teaData=[]
-let indexofTea=0
+let teaData = []
+let indexofTea = 0
 
-app.post('/teas',(req,res)=>{
-    
-    let {name,price} = req.body
-    teaData.push({id:indexofTea++,name:name,price:price})
+app.post('/teas', (req, res) => {
+
+    let { name, price } = req.body
+    teaData.push({ id: indexofTea++, name: name, price: price })
     res.status(200).send(teaData)
 })
 
-app.get('/teas',(req,res)=>{
+app.get('/teas', (req, res) => {
     res.status(200).send(teaData)
 })
 
-app.get('/teas/:id',(req,res)=>{
-    const tea=teaData.find(t=> t.id === Number(req.params.id))
+app.get('/teas/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === Number(req.params.id))
     if (!tea) {
         return res.status(404).send("tea not found")
-    }else{
+    } else {
         return res.status(200).send(tea)
     }
 })
 
-app.put('/teas/:id',(req,res)=>{
-    teaData.forEach(t=>{
+app.put('/teas/:id', (req, res) => {
+    teaData.forEach(t => {
         if (t.id === Number(req.params.id)) {
-            const {name,price}=req.body
-            t.name=name
-            t.price=price
+            const { name, price } = req.body
+            t.name = name
+            t.price = price
             console.log("hello");
-            
+
             return res.status(200).send(teaData)
         }
     })
-    
+
 })
 
-app.delete('/teas/:id',(req,res)=>{
-    teaData=teaData.filter(t => t.id !==Number(req.params.id) ? t : null)
+app.delete('/teas/:id', (req, res) => {
+    teaData = teaData.filter(t => t.id !== Number(req.params.id) ? t : null)
+    return res.status(200).send(teaData)
+})
+
+app.delete("/teas", (req, res) => {
+    teaData = []
     return res.status(200).send(teaData)
 })
 
 
-
-app.listen(port,hostname,()=>{
+app.listen(port, hostname, () => {
     console.log("server is listeneni");
-    
+
 })
+
+
+//muneeb
